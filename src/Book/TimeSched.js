1a6a76c1a6309d9de10f77e2cb5c21140832be2a
jonayoung <young.jon@icloud.com>
2020-08-20 22:58:50 -0400

    customized DRF API booking list to date-specific, setup daily schedule render to account for bookings for the day and no bookings for the day

--- src/Book/TimeSched.js
+++ src/Book/TimeSched.js
@@ -8,77 +8,110 @@ function TimeSched({ curDate }) {
   const [courtBookings, setCourtBookings] = useState([{}]);
   const dateFormat = "yyyy-MM-d-p";
   const rows = [];
-  let curHours = setHours(curDate, 7);
-  let curTime = setMinutes(curHours, 0);
-  let endHours = setHours(curDate, 22);
-  let endTime = setMinutes(endHours, 0);
-  let days = [];
+  let timeSlots = [];
+  let curTime = setMinutes(setHours(curDate, 7), 0);
+  let endTime = setMinutes(setHours(curDate, 22), 0);
 
-  //If the component updates or mounts, call fetchBookings()
-  useEffect(() => {
-    fetchBookings();
-  }, []);
-
-  //Pulls the list of tennis bookings
-  //TODO: Currently pulls ALL tennis bookings...can you either pull all bookings on the day of
-  //TODO: OR pull the bookings of the specific curTime which is a date and time element...
+  //Pulls booking list from DRF API on curDate
   const fetchBookings = () => {
-    console.log("Fetching...");
+    const urlDateFormat = "yyyy-MM-d";
 
-    fetch("http://127.0.0.1:8000/api/tbook-list/")
+    fetch(
+      `http://127.0.0.1:8000/api/tbook-list/${format(curDate, urlDateFormat)}/`
+    )
       .then((response) => response.json())
       .then((data) => setCourtBookings(data));
   };
 
-  //Number of rows is denoted by the while loop (below)...could just set another for loop with the time counter
+  //Every time the curDate changes or updates, run fetchBookings
+  useEffect(() => {
+    fetchBookings();
+  }, [curDate]);
+
+  //Sets # of rows from curTime & endTime (Club's Hours of Operations)
   while (curTime <= endTime) {
-    //Number of columns as per the Hours Column to the 4 Courts
+    //Sets # of columns, in this case 5 for time and 4 courts
     for (let i = 0; i < 5; i++) {
+      //First column is the time
       if (i === 0) {
-        days.push(
-          <div
-            className="sched__col cell coltime"
-            key={`Time-${format(curTime, "p")}`}
-          >
-            {format(curTime, "p")}
+        const timeCell = format(curTime, "p");
+        timeSlots.push(
+          <div className="sched__col cell coltime" key={`Time-${timeCell}`}>
+            {timeCell}
           </div>
         );
       } else {
-        //TODO: Set if statement if (court booking on curTime & on specific court), then display from DRF API
-        //TODO: curtime match with court_date & court_time on django & specific court off i and court_number
-        {
-          /*
-        if (format(curTime, someDateFormat) "search is true within fetchTasksData of court_date & court_time"
-            && strVar(i) "search is true within fetchTasksData of court_number")
-        ? days.push('Booked Court Information')
-        : days.push('Book a Court Cell Below')
-        */
-        }
-        days.push(
-          <div
-            className="sched__col cell"
-            key={`${i}--${format(curTime, dateFormat)}`}
-          >
-            Book a Court!
-            <button
-              className="sched__button"
-              //TODO: may have to change name for easier pickup in a function on click
-              name={`${i}--${format(curTime, dateFormat)}`}
-              value={`${i}--${format(curTime, dateFormat)}`}
+        const courtNum = i.toString();
+        const courtTime = format(curTime, "p");
+        //Used as a value to prep DRF API form for booking
+        const courtCode = courtNum + "--" + format(curTime, dateFormat);
+        const bookACourt = () => {
+          timeSlots.push(
+            <div
+              className="sched__col cell"
+              key={`${i}--${format(curTime, dateFormat)}`}
             >
-              Click here!
-            </button>
-          </div>
-        );
+              <button
+                className="sched__button"
+                name={courtCode}
+                value={courtCode}
+              >
+                Book a Court!
+              </button>
+            </div>
+          );
+        };
+
+        //If no court bookings on the day, fill every cell with bookACourt
+        if (courtBookings.length === 0) {
+          bookACourt();
+          //If there are court bookings on the day
+        } else {
+          //Cycles through each court booking to match to time and court cell
+          for (let bx = 0; bx <= courtBookings.length; bx++) {
+            let book = courtBookings[bx];
+            //If courtBooking object is same time as row's time
+            if (courtTime === book.court_time) {
+              //If courtBooking object has same time and court number as row
+              if (courtNum === book.court_number) {
+                timeSlots.push(
+                  <div
+                    className="sched__col cell"
+                    key={`${i}--${format(curTime, dateFormat)}`}
+                  >
+                    {courtBookings[bx].court_date}
+                    <br />
+                    Court {courtBookings[bx].court_number}
+                    <br />
+                    Booked by - {courtBookings[bx].author}
+                  </div>
+                );
+                break;
+                //If courtBooking object has same time but not same court number
+              } else {
+                bookACourt();
+                break;
+              }
+              //If cycles through last object in courtBooking & no courtTime match
+            } else if (bx == courtBookings.length - 1) {
+              bookACourt();
+              break;
+              //Makes sure to cycle through whole courtBooking array for match
+            } else {
+              continue;
+            }
+          }
+        }
       }
     }
 
     rows.push(
       <div className="sched__row" key={curTime}>
-        {days}
+        {timeSlots}
       </div>
     );
-    days = [];
+    //Resets time slots
+    timeSlots = [];
     curTime = addHours(curTime, 1);
   }
   return <div className="sched__body">{rows}</div>;
